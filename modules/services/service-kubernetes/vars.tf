85d1ee3606d3fbc0ede9889132ae5c76bf46b81c
bic-harness <bogdan.catana@harness.io>
2023-05-22 15:22:29 +0100

    added vars

--- modules/services/service-kubernetes/vars.tf
+++ modules/services/service-kubernetes/vars.tf
@@ -2,6 +2,10 @@ variable "team_name" {
   description = "The Team Name."
 }
 
+variable "organization_id" {
+  description = "The Harness Organization ID."
+}
+
 variable "name" {
   description = "The name of the service."
 }
