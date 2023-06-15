183e995da739dbb0b7e79541f3e9051273cface7
bic-harness <bogdan.catana@harness.io>
2023-06-15 10:29:41 +0100

    Update main.tf

--- modules/infrastructure-definitions/main.tf
+++ modules/infrastructure-definitions/main.tf
@@ -25,6 +25,7 @@ module "kubernetes" {
   organization_id = data.harness_platform_organization.this.id
   team_name       = var.team_name
   name            = each.key
+  identifier      = "${each.key}_${random_string.this.result}"
   env             = "${each.value.environment}__${var.environment_random_string}"
   type            = each.value.type
   connector       = "${each.value.connector}_${var.connector_random_string}"
