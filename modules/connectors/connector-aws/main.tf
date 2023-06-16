24474603a9339cb8da429c323bb79ad75bfb6f7a
bic-harness <bogdan.catana@harness.io>
2023-06-16 15:59:02 +0100

    added AWS connectors

--- modules/connectors/connector-aws/main.tf
+++ modules/connectors/connector-aws/main.tf
@@ -1,16 +1,3 @@
-/*
-locals {
-  config_path            = "${path.module}/../../../config"
-  global_config          = yamldecode(file("${var.config_path}/values.yaml"))
-  team_config            = yamldecode(file("${var.config_path}/${var.team_name}/values.yaml"))
-  service_config         = yamldecode(file("${var.config_path}/${var.team_name}/services.yaml"))
-  var_types = {
-    "text" : "TEXT",
-    "secret" : "ENCRYPTED_TEXT",
-  }
-}
-*/
-
 data "harness_platform_organization" "this" {
   identifier = var.organization_id
 }
@@ -20,11 +7,12 @@ data "harness_platform_project" "this" {
   org_id = data.harness_platform_organization.this.id
 }
 
-resource "harness_platform_connector_kubernetes" "this" {
+resource "harness_platform_connector_aws" "this" {
   identifier  = var.identifier
   name        = var.name
 
-  inherit_from_delegate {
-    delegate_selectors = ["${var.team_name}-harness-delegate"]
+  manual {
+    access_key_ref = account.nextGenAWSAccessKey
+    secret_key_ref = account.nextGenSecretKey
   }
 }
--- modules/connectors/main.tf
+++ modules/connectors/main.tf
@@ -1,6 +1,7 @@
 locals {
   config_path            = "${path.module}/../../config"
   k8s_connector_config   = yamldecode(file("${local.config_path}/${var.team_name}/K8s/connectors.yaml"))
+  aws_connector_config   = yamldecode(file("${local.config_path}/${var.team_name}/AWS/connectors.yaml"))
 }
 
 data "harness_platform_organization" "this" {
@@ -22,6 +23,16 @@ resource "random_string" "this" {
 module "connector_kubernetes" {
   for_each        = local.k8s_connector_config
   source          = "./connector-kubernetes"
+  organization_id = data.harness_platform_organization.this.id
+  team_name       = var.team_name
+  name            = each.key
+  identifier      = "${each.key}_${random_string.this.result}"
+}
+
+
+module "connector_kubernetes" {
+  for_each        = local.aws_connector_config
+  source          = "./connector-aws"
   organization_id = data.harness_platform_organization.this.id
   team_name       = var.team_name
   name            = each.key
