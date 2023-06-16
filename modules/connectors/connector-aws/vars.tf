839392f5bdd40ddccaf68f863a59b04c90a34a7a
bic-harness <bogdan.catana@harness.io>
2023-06-16 15:52:30 +0100

    added new aws connector module

---
+++ modules/connectors/connector-aws/vars.tf
@@ -0,0 +1,15 @@
+variable "team_name" {
+  description = "The Team Name."
+}
+
+variable "organization_id" {
+  description = "The Harness Organization ID."
+}
+
+variable "name" {
+  description = "The name of the resource."
+}
+
+variable "identifier" {
+  description = "The identifier of the resource."
+}
