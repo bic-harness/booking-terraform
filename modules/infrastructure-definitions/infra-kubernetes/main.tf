d2e13d589eb47a7ee638671e47a348e801605f76
bic-harness <bogdan.catana@harness.io>
2023-06-16 12:01:07 +0100

    Update main.tf

--- modules/infrastructure-definitions/infra-kubernetes/main.tf
+++ modules/infrastructure-definitions/infra-kubernetes/main.tf
@@ -2,14 +2,8 @@ data "harness_platform_organization" "this" {
   identifier = var.organization_id
 }
 
-data "harness_platform_project" "this" {
-  name = "${var.team_name}"
-  org_id = data.harness_platform_organization.this.id
-}
-
 resource "harness_platform_infrastructure" "this" {
   org_id          = data.harness_platform_organization.this.id
-  project_id      = data.harness_platform_project.this.id
   name            = var.name
   identifier      = var.identifier
   env_id          = var.env
@@ -23,7 +17,6 @@ infrastructureDefinition:
   description: ""
   tags: {}
   orgIdentifier: ${data.harness_platform_organization.this.id}
-  projectIdentifier: ${data.harness_platform_project.this.id}
   environmentRef: ${var.env}
   deploymentType: ${var.type}
   type: ${var.type == "Kubernetes" ? "KubernetesDirect" : var.type}
