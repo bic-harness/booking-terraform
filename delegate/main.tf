1a94fc0a0e0fd038a3b8bca6f5a9b42731b5fc14
Patryk Pilecki <patryk.pilecki@harness.io>
2024-05-21 12:55:14 +0100

    added vars.tf

--- delegate/main.tf
+++ delegate/main.tf
@@ -1,4 +1,6 @@
-module "modules" {
+module "k8s_connector" {
   source  = "app.terraform.io/empire/modules/harness//modules/connectors"
   version = "1.0.0"
+  team_name = var.team_name
+  organization_id = var.organization_id
 }
